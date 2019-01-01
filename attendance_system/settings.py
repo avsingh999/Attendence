bc41d6a9ca3a463dd8bbfe00f17096beea26ad69
gjergjk71 <gjergjk71@gmail.com>
2019-01-01 15:24:21 +0100

    Added 'api' and 'rest_framework' to INSTALLED_APPS

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home',
+    'api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
