9e9370125bfc4958de02171a3ae4c8e16bf10913
gjergjk71 <gjergjk71@gmail.com>
2019-01-01 15:27:29 +0100

    Registered AttendancesViewSet to router

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'professers', views.FacultysViewSet)
 router.register(r'students', views.StudentsViewSet)
+router.register(r'attendances',views.AttendancesViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
