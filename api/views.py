21e4085ab44420b60d6e9feda3bbcc7340d06732
gjergjk71 <gjergjk71@gmail.com>
2019-01-01 15:23:20 +0100

    Created FacultysViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .serializers import FacultysSerializer,StudentsSerializer
+from home.models import Facultys,Students
 # Create your views here.
+
+class FacultysViewSet(ModelViewSet):
+	queryset = Facultys.objects.all()
+	serializer_class = FacultysSerializer
+
