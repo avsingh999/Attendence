c023a97c392fdd49c5363547f5d00f811effdcdf
avsingh999 <201651011@iiitvadodara.ac.in>
2019-05-19 23:22:01 +0530

    add ng app

--- attendance-frontend/src/app/app.module.ts
+++ attendance-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AttendanceComponent } from './attendance/attendance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AttendanceComponent
   ],
   imports: [
     BrowserModule,
